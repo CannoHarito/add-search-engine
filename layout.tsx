289fae9b386d596e3c8fd4519c393b6fd26ccf9e
CannoHarito <45878603+CannoHarito@users.noreply.github.com>
2025-02-26 18:01:30 +0900

    og;imageにサイズ表記してみる

--- layout.tsx
+++ layout.tsx
@@ -32,6 +32,9 @@ const Layout = (props: SiteData) =>
 <title>Add Search Engine</title>
 <meta name="description" content="Desktop用Firefoxに検索エンジンを追加するのを、お手伝いします" />
 <meta property="og:image" content="https://i.gyazo.com/eb63fdc310a90abf24031768cdfa7718.png" />
+<meta property="og:image:type" content="image/png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
 ${<Style>{globalClass}</Style>}
 ${props.head}
 
